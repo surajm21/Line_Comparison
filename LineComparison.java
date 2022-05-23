c4774fbef68fbf0552663e10fc464fb34f9fd6d6
suraj <surajmore2143@gmail.com>
2022-05-23 18:53:14 +0530

    Added Equality of line feature

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,7 @@
 package com.bridglabz;
 
 import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,5 +16,22 @@ public class LineComparison {
         int y2= sc.nextInt();
         double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
         System.out.println("length of line1 is:"+length1);
+
+        System.out.println("enter the coordinates on x3");
+        int x3= sc.nextInt();
+        System.out.println("enter the coordinates on y3");
+        int y3= sc.nextInt();
+        System.out.println("enter the coordinates on x4");
+        int x4= sc.nextInt();
+        System.out.println("enter the coordinates on y4");
+        int y4= sc.nextInt();
+        double length2 = Math.sqrt((x3-x4)*(x3-x4) + (y3-y4)*(3-y4));
+        System.out.println("length of line2 is:"+length2);
+
+             String s1=String.valueOf(length1);
+             String s2=String.valueOf(length2);
+
+        System.out.println("the length of two lines are equal or not:"+s1.equals(s2));
+
     }
 }
