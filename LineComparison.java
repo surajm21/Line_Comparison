26f3a16d45e524cde29e286328ae57eef3ac1712
suraj <surajmore2143@gmail.com>
2022-05-31 11:21:51 +0530

    Added to Object oriented concept of line

--- LineComparison.java
+++ LineComparison.java
@@ -3,6 +3,21 @@ package com.bridglabz;
 import java.util.Scanner;
 
 public class LineComparison {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LineComparison(int x1,int x2,int y1,int y2){
+        this.x1=x1;
+        this.x2=x2;
+        this.y1=y1;
+        this.y2=y2;
+    }
+    public double length(){
+        double lengthOfLine= Math.pow(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)),0.5);
+        return lengthOfLine;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -14,8 +29,7 @@ public class LineComparison {
         int x2= sc.nextInt();
         System.out.println("enter the coordinates on y2");
         int y2= sc.nextInt();
-        double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("length of line1 is:"+length1);
+
 
         System.out.println("enter the coordinates on x3");
         int x3= sc.nextInt();
@@ -25,20 +39,29 @@ public class LineComparison {
         int x4= sc.nextInt();
         System.out.println("enter the coordinates on y4");
         int y4= sc.nextInt();
-        double length2 = Math.sqrt((x3-x4)*(x3-x4) + (y3-y4)*(3-y4));
-        System.out.println("length of line2 is:"+length2);
 
-            if (Double.compare(length1, length2) == 0) {
-                System.out.println("Both the lines are equal in length");
-            }
-            else if (Double.compare(length1, length2) < 0) {
-                System.out.println("The second line greater then first line");
-            }
-            else {
-                System.out.println("The first line is greater then second line");
-        }
+        LineComparison l1 = new LineComparison(x1,y1,x2,y2);
+        System .out.println("length of line1 is:"+l1.length());
+        LineComparison l2 = new LineComparison(x3,y3,x4,y4);
+        System.out.println(" length of  line2  is "+l2.length());
 
+        int val1 = (int) l1.length();
+        int val2 = (int) l2.length();
 
+        Integer length1 = Integer.valueOf(val1);
+        Integer length2 = Integer.valueOf(val2);
+        
+        if(length1.equals(length2))
+            System.out.println("The lines are equal!!!");
+        else
+            System.out.println("The lines are not equal!!!");
 
+        int result=length1.compareTo(length2);
+        if(result>0)
+            System.out.println("line1 is greater than line2!!!");
+        else if(result<0)
+            System.out.println("Line2 is greater than line1!!!");
+        else
+            System.out.println("line1 is greater than line2!!!");
     }
 }
